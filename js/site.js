a0dcb6829eca3bef74590339b3ef09cd13195ba0
Drew Hackelman (dh694n) <dh694n@att.com>
2018-07-16 13:23:48 -0400

    Upload working html css and js for deck widget

---
+++ js/site.js
@@ -0,0 +1,620 @@
+
+
+var json = [
+  {
+    "Category": "prep",
+    "Name": "Mobilization",
+    "Desc": "Fee for Mobilization",
+    "Units": 1,
+    "Unit_Cost": 500,
+    "DIY": 0,
+    "Happening": 1,
+    "url": ""
+  },
+  {
+    "Category": "prep",
+    "Name": "Permits",
+    "Desc": "Durham City Permit",
+    "Units": 1,
+    "Unit_Cost": 450,
+    "DIY": 0,
+    "Happening": 1,
+    "url": ""
+  },
+  {
+    "Category": "prep",
+    "Name": "Transit / Site",
+    "Desc": "Verify grading of steps",
+    "Units": 1,
+    "Unit_Cost": 320,
+    "DIY": 0,
+    "Happening": 1,
+    "url": ""
+  },
+  {
+    "Category": "prep",
+    "Name": "Diana @ Site",
+    "Desc": "Diana visits throughout",
+    "Units": 3,
+    "Unit_Cost": 120,
+    "DIY": 0,
+    "Happening": 1,
+    "url": ""
+  },
+  {
+    "Category": "prep",
+    "Name": "Driveway dressing",
+    "Desc": "Refinish back driveway at the end fo the project",
+    "Units": 42,
+    "Unit_Cost": 50,
+    "DIY": 0,
+    "Happening": 1,
+    "url": ""
+  },
+  {
+    "Category": "prep",
+    "Name": "Mimosa",
+    "Desc": "removal",
+    "Units": 1,
+    "Unit_Cost": 120,
+    "DIY": 1,
+    "Happening": 0,
+    "url": ""
+  },
+  {
+    "Category": "prep",
+    "Name": "Lawn Demo",
+    "Desc": "Scraping, rough grading, excavation, loading, hauling, organic materials, scraping of lawn",
+    "Units": 3,
+    "Unit_Cost": 3000,
+    "DIY": 0,
+    "Happening": "?",
+    "url": ""
+  },
+  {
+    "Category": "demo",
+    "Name": "landing / steps",
+    "Desc": "Demo existing landing and wooden structure",
+    "Units": 1,
+    "Unit_Cost": 600,
+    "DIY": 1,
+    "Happening": 1,
+    "url": ""
+  },
+  {
+    "Category": "demo",
+    "Name": "concrete landing",
+    "Desc": "Remove weird concrete steps under landing",
+    "Units": 1,
+    "Unit_Cost": "?",
+    "DIY": 1,
+    "Happening": 1,
+    "url": ""
+  },
+  {
+    "Category": "demo",
+    "Name": "brick columns",
+    "Desc": "Demo brick columns",
+    "Units": 2,
+    "Unit_Cost": 250,
+    "DIY": 1,
+    "Happening": 1,
+    "url": ""
+  },
+  {
+    "Category": "demo",
+    "Name": "brick edging",
+    "Desc": "remove bricks around edge of yard",
+    "Units": 80,
+    "Unit_Cost": 8,
+    "DIY": 1,
+    "Happening": 1,
+    "url": ""
+  },
+  {
+    "Category": "demo",
+    "Name": "shed",
+    "Desc": "Demolish and dispose of shed",
+    "Units": 1,
+    "Unit_Cost": 600,
+    "DIY": 1,
+    "Happening": 1,
+    "url": ""
+  },
+  {
+    "Category": "demo",
+    "Name": "brick pavers",
+    "Desc": "Remove and store brick pavers in  back yard",
+    "Units": 40,
+    "Unit_Cost": 3,
+    "DIY": 1,
+    "Happening": 1,
+    "url": ""
+  },
+  {
+    "Category": "grading",
+    "Name": "Fill Dirt",
+    "Desc": "Fill dirt for new lawn to cover most of yard",
+    "Units": 20,
+    "Unit_Cost": 40,
+    "DIY": 0,
+    "Happening": 0,
+    "url": ""
+  },
+  {
+    "Category": "grading",
+    "Name": "Patio",
+    "Desc": "Grading and dirt for new patio area",
+    "Units": 8,
+    "Unit_Cost": 40,
+    "DIY": 0,
+    "Happening": 0,
+    "url": ""
+  },
+  {
+    "Category": "paving",
+    "Name": "patio stones",
+    "Desc": "fossil beige antiqued pavers + felt, border, edging, sand",
+    "Units": 240,
+    "Unit_Cost": 20,
+    "DIY": 0,
+    "Happening": 0,
+    "url": ""
+  },
+  {
+    "Category": "paving",
+    "Name": "walkway",
+    "Desc": "from parking pad to steps - pavers, crush, felt, edging",
+    "Units": 32,
+    "Unit_Cost": "?",
+    "DIY": 0,
+    "Happening": 0,
+    "url": ""
+  },
+  {
+    "Category": "paving",
+    "Name": "4' steppers",
+    "Desc": "southern stone supply",
+    "Units": 8,
+    "Unit_Cost": 300,
+    "DIY": 0,
+    "Happening": 0,
+    "url": ""
+  },
+  {
+    "Category": "paving",
+    "Name": "under deck work",
+    "Desc": "landscape fabric, stones, work space under deck",
+    "Units": 150,
+    "Unit_Cost": 8,
+    "DIY": 0,
+    "Happening": 0,
+    "url": ""
+  },
+  {
+    "Category": "paving",
+    "Name": "under sun room",
+    "Desc": "re-set pavers, gravel, edging",
+    "Units": 170,
+    "Unit_Cost": 4,
+    "DIY": 1,
+    "Happening": 1,
+    "url": ""
+  },
+  {
+    "Category": "paving",
+    "Name": "crawl space entry",
+    "Desc": "pavers, brick, etc.",
+    "Units": 16,
+    "Unit_Cost": 15,
+    "DIY": 0,
+    "Happening": 0,
+    "url": ""
+  },
+  {
+    "Category": "carpentry",
+    "Name": "kiln wood",
+    "Desc": "Kiln dried",
+    "Units": 168,
+    "Unit_Cost": 32,
+    "DIY": 0,
+    "Happening": 1,
+    "url": ""
+  },
+  {
+    "Category": "carpentry wood",
+    "Name": "cheap wood",
+    "Desc": "salty wood from HD",
+    "Units": 168,
+    "Unit_Cost": 25,
+    "DIY": 0,
+    "Happening": 0,
+    "url": ""
+  },
+  {
+    "Category": "capentry",
+    "Name": "composite wood",
+    "Desc": "Azek composite (banded with inset)",
+    "Units": 168,
+    "Unit_Cost": 50,
+    "DIY": 0,
+    "Happening": 0,
+    "url": ""
+  },
+  {
+    "Category": "carpentry",
+    "Name": "kiln steps / railing",
+    "Desc": "steps / landing / railing for deck",
+    "Units": 1,
+    "Unit_Cost": 1400,
+    "DIY": 0,
+    "Happening": 1,
+    "url": ""
+  },
+  {
+    "Category": "carpentry",
+    "Name": "cheap steps / railing",
+    "Desc": "steps / landing / railing for deck",
+    "Units": 1,
+    "Unit_Cost": 1800,
+    "DIY": 0,
+    "Happening": 0,
+    "url": ""
+  },
+  {
+    "Category": "carpentry",
+    "Name": "composite steps / railing",
+    "Desc": "steps / landing / railing for deck",
+    "Units": 1,
+    "Unit_Cost": 2200,
+    "DIY": 0,
+    "Happening": 0,
+    "url": ""
+  },
+  {
+    "Category": "carpentry",
+    "Name": "Lathe",
+    "Desc": "vertical or horizontal lathe to buffer view of deck underside",
+    "Units": 3,
+    "Unit_Cost": 400,
+    "DIY": 0,
+    "Happening": 0,
+    "url": ""
+  },
+  {
+    "Category": "carpentry wood",
+    "Name": "Fence Gates",
+    "Desc": "repair hinges, anti-sag kits",
+    "Units": 2,
+    "Unit_Cost": 120,
+    "DIY": 0,
+    "Happening": 0,
+    "url": ""
+  },
+  {
+    "Category": "stain",
+    "Name": "deck and steps",
+    "Desc": "stain and protective coats on deck and steps",
+    "Units": 6,
+    "Unit_Cost": 40,
+    "DIY": 0,
+    "Happening": 1,
+    "url": ""
+  },
+  {
+    "Category": "fence",
+    "Name": "back panel",
+    "Desc": "remove for access",
+    "Units": 1,
+    "Unit_Cost": 300,
+    "DIY": 0,
+    "Happening": 1,
+    "url": ""
+  },
+  {
+    "Category": "fence",
+    "Name": "pickets",
+    "Desc": "repair warped pickets",
+    "Units": 6,
+    "Unit_Cost": 40,
+    "DIY": 0,
+    "Happening": 0,
+    "url": ""
+  },
+  {
+    "Category": "drainage",
+    "Name": "4\" corrugated pipe",
+    "Desc": "install pipe",
+    "Units": 54,
+    "Unit_Cost": 12,
+    "DIY": "",
+    "Happening": "",
+    "url": ""
+  },
+  {
+    "Category": "drainage",
+    "Name": "6\" corrugated pipe",
+    "Desc": "install pipe",
+    "Units": 106,
+    "Unit_Cost": 14,
+    "DIY": "",
+    "Happening": "",
+    "url": ""
+  },
+  {
+    "Category": "electrical",
+    "Name": "Up Light",
+    "Desc": "CM 115A Antique-Up Light",
+    "Units": 1,
+    "Unit_Cost": 234,
+    "DIY": 0,
+    "Happening": 0,
+    "url": ""
+  },
+  {
+    "Category": "electrical",
+    "Name": "Path Lights",
+    "Desc": "CM 715 CGA Antique Path Lights",
+    "Units": 8,
+    "Unit_Cost": 216,
+    "DIY": 0,
+    "Happening": 0,
+    "url": ""
+  },
+  {
+    "Category": "electrical",
+    "Name": "Transformer",
+    "Desc": "CM 300W 1215 TRX stainless steel transformer",
+    "Units": 1,
+    "Unit_Cost": 540,
+    "DIY": 0,
+    "Happening": 0,
+    "url": ""
+  },
+  {
+    "Category": "electrical",
+    "Name": "Electrician Fees",
+    "Desc": "Fees for Electrician",
+    "Units": 1,
+    "Unit_Cost": 640,
+    "DIY": 0,
+    "Happening": 0,
+    "url": ""
+  },
+  {
+    "Category": "electrical",
+    "Name": "Misc.",
+    "Desc": "Waterproof receptacles, wirenuts, plates, switches, pedestals, etc.",
+    "Units": 1,
+    "Unit_Cost": 360,
+    "DIY": 0,
+    "Happening": 0,
+    "url": ""
+  },
+  {
+    "Category": "misc",
+    "Name": "spraying",
+    "Desc": "Round Up Pro + liquid Scythe",
+    "Units": 1,
+    "Unit_Cost": 120,
+    "DIY": 0,
+    "Happening": 0,
+    "url": ""
+  },
+  {
+    "Category": "misc",
+    "Name": "amenified soils",
+    "Desc": "amenified soils",
+    "Units": 12,
+    "Unit_Cost": 50,
+    "DIY": 0,
+    "Happening": 0,
+    "url": ""
+  },
+  {
+    "Category": "misc",
+    "Name": "Sod",
+    "Desc": "tri-blended fescue sod, pelletized dolomitic lime, golf green grade slow release fertilizer, triple super phosphate",
+    "Units": 178,
+    "Unit_Cost": 7,
+    "DIY": 0,
+    "Happening": 0,
+    "url": ""
+  },
+  {
+    "Category": "misc",
+    "Name": "landfill fees",
+    "Desc": "(deck steps, bricks, concrete, shed, columns)",
+    "Units": 1,
+    "Unit_Cost": 720,
+    "DIY": 1,
+    "Happening": 1,
+    "url": ""
+  },
+  {
+    "Category": "misc",
+    "Name": "bark mulch",
+    "Desc": "triple shredded hardwood back",
+    "Units": 8,
+    "Unit_Cost": 70,
+    "DIY": 0,
+    "Happening": 0,
+    "url": ""
+  },
+  {
+    "Category": "trees",
+    "Name": "GINKGO BILOBA",
+    "Desc": "MAIDENHAIR TREE",
+    "Units": 1,
+    "Unit_Cost": 320,
+    "DIY": 0,
+    "Happening": 0,
+    "url": "https://img2.cgtrader.com/items/745602/ff3d96b827/large/ginkgo-tree-ginkgo-biloba-7m-3d-model-max-obj-fbx-c4d-ma-mb.jpg"
+  },
+  {
+    "Category": "shrubs",
+    "Name": "HYDRANGEA PANICULATA 'VANILLA STRAWBERRY'",
+    "Desc": "PANICLE HYDRANGEA",
+    "Units": 4,
+    "Unit_Cost": 60,
+    "DIY": 0,
+    "Happening": 0,
+    "url": "http://www.johnson-nursery.com/Images/johnsonnursery/product-images/Hydrangea%20Fire%20Light%203_trcjist0x.jpg"
+  },
+  {
+    "Category": "shrubs",
+    "Name": "LOROPETALUM 'CRIMSON FIRE'",
+    "Desc": "LOROPETALUM",
+    "Units": 11,
+    "Unit_Cost": 72,
+    "DIY": 0,
+    "Happening": 0,
+    "url": "https://cdn.shopify.com/s/files/1/0207/5750/products/loropetalum_daruma_large.jpg?v=1521739140"
+  },
+  {
+    "Category": "shrubs",
+    "Name": "LOROPETALUM 'DARUMA' DWARF",
+    "Desc": "LOROPETALUM",
+    "Units": 9,
+    "Unit_Cost": 64,
+    "DIY": 0,
+    "Happening": 0,
+    "url": "https://cdn.shopify.com/s/files/1/0207/5750/products/loropetalum_daruma_large.jpg?v=1521739140"
+  },
+  {
+    "Category": "shrubs",
+    "Name": "ROSA DRIFT 'NOVAROSPOP'",
+    "Desc": "POPCORN DRIFT ROSE",
+    "Units": 8,
+    "Unit_Cost": 52,
+    "DIY": 0,
+    "Happening": 0,
+    "url": "https://st.hzcdn.com/simgs/fc3205f50532b187_8-5669/home-design.jpg"
+  },
+  {
+    "Category": "shrubs",
+    "Name": 444,
+    "Desc": "CLEYERA",
+    "Units": 3,
+    "Unit_Cost": "",
+    "DIY": 0,
+    "Happening": 0,
+    "url": "https://target.scene7.com/is/image/Target/52685707?wid=488&hei=488&fmt=pjpeg"
+  },
+  {
+    "Category": "shrubs",
+    "Name": "THUJA 'TECHNITO'",
+    "Desc": "COLUMNAR ARBORVITAE",
+    "Units": 1,
+    "Unit_Cost": 72,
+    "DIY": 0,
+    "Happening": 0,
+    "url": "https://plants.jimwhitingnursery.com/Plant-Name/_ccLib/image/plantstock/CARD-111619.jpg"
+  },
+  {
+    "Category": "shrubs",
+    "Name": "VIBURNUM P. 'KILIMANJARO'",
+    "Desc": "KILIMANJARO VIBURNUM",
+    "Units": 3,
+    "Unit_Cost": 52,
+    "DIY": 0,
+    "Happening": 0,
+    "url": "https://s3.amazonaws.com/finegardening.s3.tauntoncloud.com/app/uploads/2018/01/23174605/041075038-01_xlg.jpg"
+  },
+  {
+    "Category": "shrubs",
+    "Name": "VIBURNUM MACR. 'STERILE'",
+    "Desc": "CHINESE SNOWBALL",
+    "Units": 2,
+    "Unit_Cost": 64,
+    "DIY": 0,
+    "Happening": 0,
+    "url": "https://image.shutterstock.com/image-photo/hydrangea-macrophylla-flower-bouquet-flowers-260nw-790666954.jpg"
+  },
+  {
+    "Category": "vines",
+    "Name": "HYDRANGEA PETIOLOARIS",
+    "Desc": "CLIMBING HYDRANGEA",
+    "Units": 3,
+    "Unit_Cost": 106,
+    "DIY": 0,
+    "Happening": 0,
+    "url": "https://www.whiteflowerfarm.com/mas_assets/cache/image/6/d/0/1744.Jpg"
+  },
+  {
+    "Category": "perrenials",
+    "Name": "ANEMONE HUPEHENSIS 'HONORINE JOBERT'",
+    "Desc": "HONORINE JOBERT ANEMONE",
+    "Units": 12,
+    "Unit_Cost": 12,
+    "DIY": 0,
+    "Happening": 0,
+    "url": "https://www.valesgreenhouse.com/images/PlantofYear2016a.jpg"
+  },
+  {
+    "Category": "perrenials",
+    "Name": "EUPATORIUM MACULATUM 'GATEWAY'",
+    "Desc": "JOE-PYE WEED",
+    "Units": 18,
+    "Unit_Cost": 12,
+    "DIY": 0,
+    "Happening": 0,
+    "url": "https://www.ebertsgreenhouse.com/_ccLib/image/plants/DETA-1139.jpg.pagespeed.ce.87-cgDGJFC.jpg"
+  },
+  {
+    "Category": "perrenials",
+    "Name": "SALVIA 'MAY NIGHT'",
+    "Desc": "MAY NIGHT SAGE",
+    "Units": 57,
+    "Unit_Cost": 12,
+    "DIY": 0,
+    "Happening": 0,
+    "url": "https://e54055a024bc6fb58d47-f7df714a3b816a175961a96ef2278d84.ssl.cf2.rackcdn.com/13178-May-Night-Salvia.jpg"
+  },
+  {
+    "Category": "perrenials",
+    "Name": "STOKESIA 'MARY GREGORY'",
+    "Desc": "STOKES ASTER",
+    "Units": 57,
+    "Unit_Cost": 12,
+    "DIY": 0,
+    "Happening": 0,
+    "url": "https://hgtvhome.sndimg.com/content/dam/images/grdn/fullset/2013/10/3/0/Original_aster-1.JPG.rend.hgtvcom.231.174.suffix/1452647146229.jpeg"
+  },
+  {
+    "Category": "perrenials",
+    "Name": "VERBENA BONARIENSIS 'LOLLIPOP'",
+    "Desc": "BRAZILIAN VERBENA",
+    "Units": 12,
+    "Unit_Cost": 20,
+    "DIY": 0,
+    "Happening": 0,
+    "url": "https://www.thompson-morgan.com/product_images/100/optimised/VERB-T57548-A_h.jpg"
+  }
+];
+
+// console.log(json); 
+
+
+var table_body = document.getElementById('table_body'); 
+
+for(i = 0; i < json.length; i++) {
+  var table_row = json[i];
+  var row = table_body.insertRow();
+  var cell0 = row.insertCell(); 
+  cell0.innerHTML = '<td>'+(i+1)+'</td>' ;
+  var cell1 = row.insertCell(); 
+  cell1.innerHTML = '<td>'+json[i].Category+'</td>' ;
+  var cell2 = row.insertCell(); 
+  cell2.innerHTML = '<td>' + json[i].Name+'</td>';
+  var cell3 = row.insertCell(); 
+  cell3.innerHTML = '<td>'+json[i].Desc+'</td>';
+  var cell4 = row.insertCell(); 
+  cell4.innerHTML = '<td>'+json[i].Units+'</td>';
+  var cell5 = row.insertCell(); 
+  cell5.innerHTML = '<td>'+json[i].Unit_Cost+'</td>';
+  var cell6 = row.insertCell(); 
+  cell6.innerHTML = '<label><input type="radio" name="diy_'+i+'" checked/><span>yes</span></label><label><input type="radio" name="diy_'+i+'"/><span>no</span></label>';
+  var cell7 = row.insertCell(); 
+  cell7.innerHTML = '<label><input type="radio" name="happening_'+i+'" checked/><span>yes</span></label><label><input type="radio" name="happening_'+i+'"/><span>no</span></label>';
+}
